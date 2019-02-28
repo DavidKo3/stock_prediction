eca601c4690b35e51a9e3720b7a2522b8a32f839
we <david@wemakeprice.com>
2019-02-28 09:53:07 +0900

    process of cleaning the data

--- src/main.py
+++ src/main.py
@@ -1,1 +1,67 @@
-print("Test")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+from scipy.optimize import minimize
+import statsmodels.tsa.api as smt
+import statsmodels.api as sm
+
+
+
+from sklearn.metrics import r2_score, median_absolute_error, mean_absolute_error
+from sklearn.metrics import median_absolute_error, mean_squared_error, mean_squared_log_error
+
+
+from tqdm import tqdm_notebook
+
+from itertools import product
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+
+DATAPATH = '../data/stock_prices_sample.csv'
+
+data = pd.read_csv(DATAPATH, index_col=['DATE'], parse_dates=['DATE'])
+data.head(10)
+
+# print(data.head(10))
+
+
+# cleaning the data
+data = data[data.TICKER != 'GEF']
+data = data[data.TYPE != 'Intraday']
+
+# print(data.head())
+
+drop_cols = ['SPLIT_RATIO', 'EX_DIVIDEND', 'ADJ_FACTOR', 'ADJ_VOLUME', 'ADJ_CLOSE', 'ADJ_LOW', 'ADJ_HIGH', 'ADJ_OPEN', 'VOLUME', 'FREQUENCY', 'TYPE', 'FIGI']
+data.drop(drop_cols, axis=1, inplace=True)
+
+
+print("---------------data---------------------")
+print(data.CLOSE)
+print("----------------------------------------")
+
+
+plt.figure(figsize=(17, 8))
+plt.plot(data.CLOSE)
+plt.title('Closing price of New Germany Fund Inc (GF)')
+plt.ylabel('Closing price ($)')
+plt.xlabel('Trading day')
+plt.grid(False)
+plt.show()
+
+
+
+
+
+
+
+
+
