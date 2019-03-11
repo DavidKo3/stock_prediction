c9f4394985e5d7c0afbea8bdd604f927f51a629b
we <david@wemakeprice.com>
2019-03-11 11:37:55 +0900

    process of cleaning the data

--- src/air_quality_prediction.py
+++ src/air_quality_prediction.py
@@ -7,7 +7,21 @@ from scipy import stats
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
-DATAPATH = 'data/AirQualityUCI.csv'
+DATAPATH = '../data/AirQualityUCI.csv'
 
 data = pd.read_csv(DATAPATH, sep=';')
-data.head()
+print(data.head())
+
+print(data.shape)
+
+
+# first get rid of all instances where there is an empty value
+data.dropna(axis=1, how='all', inplace=True)
+data.dropna(axis=0, how='all', inplace=True)
+
+print(data.shape)
+
+# Data cleaning and feature engineering
+data['Date'] = pd.to_datetime(data['Date'])
+
+print(data.iloc[:, 2:])
