aa36398fe320a8c1fff158140a8e0de30f0f0261
we <david@wemakeprice.com>
2019-03-12 12:14:00 +0900

    process of cleaning the data

--- src/air_quality_prediction.py
+++ src/air_quality_prediction.py
@@ -10,14 +10,14 @@ import matplotlib.pyplot as plt
 DATAPATH = '../data/AirQualityUCI.csv'
 
 data = pd.read_csv(DATAPATH, sep=';')
-print(data.head())
+
 
 print(data.shape)
 
 
 # first get rid of all instances where there is an empty value
-data.dropna(axis=1, how='all', inplace=True)
-data.dropna(axis=0, how='all', inplace=True)
+data.dropna(axis=1, how='all', inplace=True) # delete column with Nan
+data.dropna(axis=0, how='all', inplace=True) # delete row with NaN
 
 print(data.shape)
 
@@ -25,3 +25,15 @@ print(data.shape)
 data['Date'] = pd.to_datetime(data['Date'])
 
 print(data.iloc[:, 2:])
+
+
+print(data.head())
+
+for col in data.iloc[:, 2:].columns:
+    print(data[col].dtypes)
+    if data[col].dtypes == object:
+        data[col] = data[col].str.replace(',', '.').astype('float')
+
+def positive_average(num):
+    return num[num > -200].mean()
+
